24f00ba0bd374b3b5ac4736f238126715d5806c4
kangismet@gmail.com <kangismet@gmail.com@f0195b55-6476-8867-e9e5-70bdf4202dbb>
2013-05-29 11:19:46 +0000

    Filter kata-kata kotor

---
+++ javascript/badwords.js
@@ -0,0 +1,31 @@
+var a = document.getElementsByClassName('comment-content');
+  for(var i=0;i<a.length;i++){
+                        a[i].innerHTML = a[i].innerHTML.replace(/ngentot/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/ngewe/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/memek/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/kontol/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/itil/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/fuck/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/tai/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/anjing/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/goblok/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/setan/gi, "kereeen");
+						a[i].innerHTML = a[i].innerHTML.replace(/tolol/gi, "kereeen");
+                        a[i].innerHTML = a[i].innerHTML.replace(/mampus/gi, "kereeen");
+
+}   
+var b = document.getElementsByClassName('widget-content');
+  for(var i=0;i<b.length;i++){
+                        b[i].innerHTML = b[i].innerHTML.replace(/ngentot/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/ngewe/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/memek/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/kontol/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/itil/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/fuck/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/tai/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/anjing/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/goblok/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/setan/gi, "kereeen");
+						b[i].innerHTML = b[i].innerHTML.replace(/tolol/gi, "kereeen");
+                        b[i].innerHTML = b[i].innerHTML.replace(/mampus/gi, "kereeen");
+} 
